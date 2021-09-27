983ab4e5b56390a85b81474a10cdd56ef9302078
Yeshey <yesheysangpo@hotmail.com>
2021-09-27 22:44:44 +0100

    more python

---
+++ Day1/python/puzzle1.py
@@ -0,0 +1,30 @@
+'''
+PEP Python 
+'''
+
+def day1(numbers: list) -> int:
+    for something1 in numbers:
+        for something2 in numbers:
+            if something1 + something2 == 2020:
+                return ([something1,something2])
+    return 0
+    
+def day2(numbers):
+    for something1 in numbers:
+        for something2 in numbers:
+            for somethin3 in  numbers:
+                if something1 + something2 + somethin3 == 2020:
+                    return ([something1,something2,somethin3])
+    return 0
+
+if __name__ == '__main__':
+    numbers = list()
+    # or numbers = []
+    with open("input.txt",'r') as f:
+        for line in f:
+            numbers.append(int(line))
+
+    liday1 = day1(numbers)
+    liday2 = day2(numbers)
+    print(str(liday1[0]) + " x " + str(liday1[1]) + " = " + str(liday1[0]*liday1[1]) )
+    print(str(liday2[0]) + " x " + str(liday2[1]) + " x " + str(liday2[2]) + " = " + str(liday2[0]*liday2[1]*liday2[2]) )
