c6d40921f54fa838d4e1716203f6deabacab8380
Yeshey <41551785+Yeshey@users.noreply.github.com>
2021-11-10 19:35:30 +0000

    day 7

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -12,6 +12,26 @@ def day6_p1(answers: list) -> int:
         yeses += len(set(group))
     return yeses
 
+def day6_p2(answers: list) -> int:
+    group_answers = []
+    i = -1
+    all_yes = 0
+    for group in answers:
+        i+=1
+        group_answers.append('')
+        for person in group:
+            for ch in person:
+                group_answers[i] += ch
+        for ch in set(group_answers[i]):
+            j=0
+            for person_answers in group:
+                j+=1
+                if ch not in person_answers:
+                    break
+                if j == len(group):
+                    all_yes += 1
+    return all_yes
+
 if __name__ == '__main__':
     with open("input.txt", 'r') as f:
         answers = [[]]
@@ -26,4 +46,4 @@ if __name__ == '__main__':
 
 
     print("Problem1 - ", day6_p1(answers))
-    print("problem2 - ", day6_p1(answers))
+    print("problem2 - ", day6_p2(answers))
