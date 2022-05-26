10739f02fff330de9b11ebbb39b313d17e08c421
Yeshey <a2020144466@isec.pt>
2022-05-26 19:23:46 +0200

    Day 2 of rust

--- rust_2021/day2/main.rs
+++ rust_2021/day2/main.rs
@@ -1,43 +1,80 @@
-use std::{io, cmp::Ordering};
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
-use rand::Rng;
+fn main() -> io::Result<()> {
+    /* This would read the whole file at once */
+    //let contents = fs::read_to_string("day2/input.txt").expect("Something went wrong reading the file");
+    //println!("With text:\n{}", contents);
 
-fn main() {
-    println!("Guess the number! (type <quit> to exit the game)");
+    let file_path = "day2/input.txt";
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    // Day 2 Part 1
+    let (depth, horizontal_distance) = day2_p1(file_path);
+    println!("depth: {} * horizontal_distance: {} = {}", depth, horizontal_distance, depth*&horizontal_distance);
 
-    loop {
-        println!("Please input your guess.");
+    // Day 2 Part 2
+    let (depth, horizontal_distance) = day2_p2(file_path);
+    println!("depth: {} * horizontal_distance: {} = {}", depth, horizontal_distance, depth*&horizontal_distance);
 
-        let mut guess = String::new();
-    
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-    
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                match guess.trim() == "quit" {
-                    true => {
-                        println!("goodbye!");
-                        break;
-                    }
-                    false => continue,
-                }
-            }
-        };
-    
-        println!("You guessed: {}", guess);
-        
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }   
+    Ok(())
+}
+
+fn day2_p1(file_to_read: &str) -> (u32, u32){
+
+    let file = File::open(file_to_read).unwrap(); // had ? instead of .unwrap() before
+    let reader = BufReader::new(file);
+
+    let mut depth = 0;
+    let mut horizontal_distance = 0;
+
+    for line in reader.lines(){
+        //let line = line?; // for error handling (?), but the function needs to return -> io::Result<()>
+        let line = line.unwrap(); // so I seemingly have to do this 
+
+        let ch = line.chars().next().unwrap();
+        let num = get_num(&line);
+        match ch {
+            'u' => depth-=num,
+            'd' => depth+=num,
+            'f' => horizontal_distance+=num,
+            _ => { println!("error!"); break; }
+        } 
     }
+    (depth,horizontal_distance)
+}
+
+fn day2_p2(file_to_read: &str) -> (u32, u32){
+
+    let file = File::open(file_to_read).unwrap(); // had ? instead of .unwrap() before
+    let reader = BufReader::new(file);
+
+    let mut depth = 0;
+    let mut horizontal_distance = 0;
+    let mut aim = 0;
+
+    for line in reader.lines(){
+        //let line = line?; // for error handling (?), but the function needs to return -> io::Result<()>
+        let line = line.unwrap(); // so I seemingly have to do this 
+
+        let ch = line.chars().next().unwrap();
+        let num = get_num(&line);
+        match ch {
+            'u' => aim-=num,
+            'd' => aim+=num,
+            'f' => {
+                horizontal_distance+=num;
+                depth += num * aim;
+            }
+            _ => { println!("error!"); break; }
+        } 
+    }
+    (depth,horizontal_distance)
+}
+
+fn get_num(str: &str) -> u32 {
+    /* to iterate over a string */
+    //for (char, index) in str.chars().enumerate(){ }
+
+    str.chars().last().unwrap() // gets last char of the string
+        .to_digit(10).unwrap() // converts to digit, unwrap always for error handling
 }
