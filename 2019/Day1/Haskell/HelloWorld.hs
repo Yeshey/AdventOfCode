c6e1c99eb5136a4690abf9efccfadbe070808232
Yeshey <yesheysangpo@hotmail.com>
2022-12-09 11:18:02 +0000

    Started to read the book
    http://learnyouahaskell.com/

--- 2019/Day1/Haskell/HelloWorld.hs
+++ 2019/Day1/Haskell/HelloWorld.hs
@@ -1,4 +1,34 @@
-module Main where
+-- https://adventofcode.com/2019
+-- Haskell book: http://learnyouahaskell.com/
 
-main :: IO ()
-main = putStrLn "Hello World"
+{-
+Testing for equality
+ghci> 5 == 5  
+True  
+ghci> 1 == 0  
+False  
+ghci> 5 /= 5  
+False  
+ghci> 5 /= 4  
+True  
+ghci> "hello" == "hello"  
+True 
+
+Surround negative numbers with parenthisis
+5 * (-3)
+-}
+
+import System.IO  
+import Control.Monad
+
+main = do  
+        let list = []
+        handle <- openFile "/mnt/DataDisk/PersonalFiles/2021/Projects/Programming/AdventOfCode/2019/Day1/Haskell/input.txt" ReadMode
+        contents <- hGetContents handle
+        let singlewords = words contents
+            list = f singlewords
+        print list
+        hClose handle
+
+f :: [String] -> [Int]
+f = map read
